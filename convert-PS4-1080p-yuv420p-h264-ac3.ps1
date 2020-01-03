c4492d98b6081b39d9f244a7c4eb37cc711e06d2
Szabolcs Nagy <ph3n0m3n4@gmail.com>
2020-01-03 15:30:53 +0100

    Update convert-PS4-1080p-yuv420p-h264-ac3.ps1
    typo

--- convert-PS4-1080p-yuv420p-h264-ac3.ps1
+++ convert-PS4-1080p-yuv420p-h264-ac3.ps1
@@ -1,1 +1,1 @@
-﻿\ffmpeg.exe -y -i "input.mkv" -s 1920:1080 -vcodec libx264 -acodec ac3 -vf format=yuv420p -profile:v high -level 4.2 "output.mkv"
+﻿ffmpeg.exe -y -i "input.mkv" -s 1920:1080 -vcodec libx264 -acodec ac3 -vf format=yuv420p -profile:v high -level 4.2 "output.mkv"
